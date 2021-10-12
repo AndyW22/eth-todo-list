b5c455bd0849493e9c08f4263b4b48bf599e8561
AndyW22 <andrew.waller24@outlook.com>
2021-10-12 20:26:22 +0100

    added toggleCompleted function and smart contract test

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,7 +10,8 @@ contract TodoList {
     }
 
     mapping(uint => Task) public tasks;
-
+    event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
     constructor() public {
         createTask("Go Shopping");
     }
@@ -18,6 +19,14 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 
 }
